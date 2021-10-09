bcc673bbc2aa8d0f5551061ad0d9337914650944
gosha kalosha <yablonskaya17421@gmail.com>
2021-10-09 15:42:40 +0300

    migration 003

--- src/main/resources/db/changelog/migrations/003-alter-recording-add-companyid.sql
+++ src/main/resources/db/changelog/migrations/003-alter-recording-add-companyid.sql
@@ -1,2 +1,7 @@
 ALTER TABLE recording
-ADD company_id BIGINT REFERENCES company (id);
+ADD company_id BIGINT
+DEFAULT (2);
+
+ALTER TABLE recording
+ADD FOREIGN KEY (company_id)
+REFERENCES company (id);
